64532ad8a9c2f4d50eb5bddb7f1bddbaa658709a
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-06-09 10:35:27 +0300

    генерация и добавление заголовков в alt

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ var generateAds = function () {
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
   var authors = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+  var headlines = ['Заголовок 1', 'Заголовок 2', 'Заголовок 3', 'Заголовок 4', 'Заголовок 5', 'Заголовок 6', 'Заголовок 7', 'Заголовок 8'];
   var offers = ['palace', 'flat', 'house', 'bungalo'];
 
   var ads = [];
@@ -19,6 +20,7 @@ var generateAds = function () {
   for (var i = 0; i < 8; i++) {
     ads.push({
       avatar: authors[i],
+      headline: headlines[i],
       type: offers[getRandomInt(0, offers.length)],
       location: {
         x: getRandomInt(0 - PIN_WIDTH / 2, document.querySelector('.map__pins').offsetWidth - PIN_WIDTH / 2),
@@ -39,6 +41,7 @@ var renderAds = function (ads) {
 
   adsElement.style = 'left: ' + ads.location.x + 'px; top: ' + ads.location.y + 'px;';
   adsElement.querySelector('img').src = ads.avatar;
+  adsElement.querySelector('img').alt = ads.headline;
 
   return adsElement;
 };
